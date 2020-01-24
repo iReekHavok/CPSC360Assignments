1b1b6831dadef8bad18714e569b2af53a1db3978
Brendan Cagampang <37314477+iReekHavok@users.noreply.github.com>
2020-01-24 00:10:33 -0600

    Final Update

--- Cagampang_Brendan_Homework1.cpp
+++ Cagampang_Brendan_Homework1.cpp
@@ -15,17 +15,19 @@ int main() {
   cout << "First to 100 wins.\n" << endl;
   cout << "I wish you the best of luck. Here we go!\n\n" << endl;
 
-  int playerScore = cpuScore = turnTotal = 0;
+  int playerScore = 0;
+  int cpuScore = 0;
+  int turnTotal = 0;
   int rollDie();
   int wgf();  // who goes first
   char roh;   // roll or hold
   srand(time(NULL));
 
-  wgf = wgf();
-  if (wgf == 0) {
-    cout << "You are Player 1" << endl;
+  int whoGoes = wgf();
+  //if (whoGoes == 0) {
+    //cout << "You are Player 1" << endl;
     while (playerScore < 100) {
-      die = rollDie();
+      int die = rollDie();
       while (die != 1) {
         cout << "Roll: " << die << endl;
         turnTotal += die;
@@ -41,7 +43,7 @@ int main() {
 
       cout << "Player 1 score: " <<  cpuScore << "\nPlayer 2 score: " << playerScore << endl;
   		cout << "\nIt is Player 2's turn." <<std::endl;
-  		die = rollDie();
+  		die;
   		turnTotal = 0;
   		turnTotal += die;
   		cout << "Roll: " << die;
@@ -58,14 +60,14 @@ int main() {
 					turnTotal += die;
 					cout << "Roll: " << die;
 
-  				if die == 1) {
+  				if (die == 1) {
   					turnTotal = 0;
   					cout << "\nTurn Total: " << turnTotal << "\nNew Score: " << playerScore << endl;
   				break;
   				}
   			} else {
-    				die = 0;
-    				turnTotal += roll;
+    				int die = 0;
+    				turnTotal += die;
     				playerScore += turnTotal;
     				cout << "Turn Total: " << turnTotal << endl;
     				break;
@@ -73,7 +75,15 @@ int main() {
   		}
 
     }
+  //}
+  int winner;
+  if (cpuScore > playerScore) {
+    winner = 1;
+  } else {
+    winner = 2;
   }
+
+  cout << "The winner is Player " << winner << endl;
 
   return 0;
 }
