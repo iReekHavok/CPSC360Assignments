3be6ff2a3d40960d29a4bbe2eb3c70afd3feafa7
Brendan Cagampang <37314477+iReekHavok@users.noreply.github.com>
2020-04-29 19:36:02 -0500

    Add files via upload

---
+++ Cagampang_Brendan_Homework9.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title> Homework 9 </title>
+  </head>
+  <body>
+    <?php
+      $nums = range(1,4);
+      $sum = array_sum($nums);
+      echo "The sum of 4 consecutive numbers from 1 to 4 is " .$result;
+    ?>
+  </body>
+</html>
